37bf7993e7dd35665f5b5f352a4385357ee08231
kkim-2 <kimkailyn247@gmail.com>
2024-09-25 10:08:15 -0400

    Update Operator.py

---
+++ Operator.py
@@ -0,0 +1,23 @@
+#Kailyn Kim 
+#9/24/24 
+#Operator Practice
+print(5*(2-3))
+print('2 + 2 =', end =' ')
+print(2+2)
+print('2 + 2 is', 2+2, 'as you can see')
+print(9/5)
+print(9.0/5)
+print(9/5.0)
+print(9.0/5.0)
+print(9//5)
+print(9.0//5)
+print(9//5.0)
+print(9.0//5.0)
+print(27//5)
+print(27%5)
+print(9//4)
+print(9%4)
+print(21//2)
+print(21%2)
+print(5//8)
+print(5%8)
